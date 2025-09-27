9deb4423ce47cc3e3a777936a3bed5da67791708
admin <pustygina.nastya@yandex.ru>
2025-09-27 19:20:15 +0300

    sout new feature

--- src/ru/rudn/cw2709/Main.java
+++ src/ru/rudn/cw2709/Main.java
@@ -4,5 +4,6 @@ package ru.rudn.cw2709;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Основная версия кода");
+        System.out.println("А это новая фича");
     }
 }
